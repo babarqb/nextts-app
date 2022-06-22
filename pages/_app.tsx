062e278931293d738ff106bea1d132b2033797a9
babarqb <babarqb@gmail.com>
2022-06-22 13:16:44 +0500

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import {Component} from "react";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function App(props: AppProps) {
+  let {Component, pageProps} = props;
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default App
